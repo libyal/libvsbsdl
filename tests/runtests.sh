a8b1551e2f974d6cf47019d731732714ca8d4e59
Joachim Metz <joachim.metz@gmail.com>
2026-06-14 07:59:18 +0200

    Replaced test scripts with autotest

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260602
+# Version: 20260609
 
 if test -f ${PWD}/libvsbsdl/.libs/libvsbsdl.1.dylib && test -f ./pyvsbsdl/.libs/pyvsbsdl.so
 then
 	install_name_tool -change /usr/local/lib/libvsbsdl.1.dylib ${PWD}/libvsbsdl/.libs/libvsbsdl.1.dylib ./pyvsbsdl/.libs/pyvsbsdl.so
 fi
 
+make check-build > /dev/null
+
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0 && test -f tests/test-suite.log
+if test ${RESULT} -ne 0
 then
-	cat tests/test-suite.log
+	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
